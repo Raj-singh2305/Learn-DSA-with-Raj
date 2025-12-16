9128ea6788adb08de7038eee6520bd43543a7547
Raj singh <dynemaster8@gmail.com>
2025-12-16 10:52:25 +0530

    Shiting with bits

---
+++ Lec 5/Left_Rightshift.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 34;
+    cout<<(a<<1)<<endl;
+    cout<<(a<<2)<<endl;
+    cout<<(21<<2)<<endl;
+    cout<<(17>>1)<<endl;
+    cout<<(a<<1)<<endl;
+    cout<<(32<<2)<<endl;
+}
