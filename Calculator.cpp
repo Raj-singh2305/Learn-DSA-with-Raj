1ebf740fb0581492c2b35b364bfd5ad7f2409f53
Raj singh <dynemaster8@gmail.com>
2026-01-19 17:32:31 +0530

    Switch Statement

---
+++ Calculator.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    char operation;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Enter an operator (+, -, *, /,%): ";
+    cin >> operation;
+    switch(operation){
+        case '+':
+            cout << "Result: " << a + b << endl;
+            break;
+        case '-':
+            cout << "Result: " << a - b << endl;
+            break;
+        case '*':
+            cout << "Result: " << a * b << endl;
+            break;
+        case '/':
+            if(b != 0){
+                cout << "Result: " << a / b << endl;
+            } else {
+                cout << "Error: Division by zero!" << endl;
+            }
+            break;
+
+        case '%':
+            if(b != 0){
+                cout << "Result: " << a % b << endl;
+            } else {
+                cout << "Error: Division by zero!" << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operator!" << endl;
+
+    }
+}
---
+++ Note.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n ;
+  cout<<"Enter ruppees to convert into notes: ";
+  cin>>n;
+
+}
