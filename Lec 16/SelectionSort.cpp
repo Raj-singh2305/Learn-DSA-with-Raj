87402a00d781fb684defee385ea66c4fc59a6c79
Raj singh <dynemaster8@gmail.com>
2026-02-06 22:46:02 +0530

    selection Sort

--- Lec 16/SelectionSort.cpp
+++ Lec 16/SelectionSort.cpp
@@ -6,7 +6,6 @@ void printingArr(int arr[], int n){
         cout<<arr[i]<<" ";
     }
 }
-//selectionSORT
 void SelectionSort(int arr[], int n) {
 
     for(int i = 0; i < n - 1; i++) {
@@ -22,7 +21,7 @@ void SelectionSort(int arr[], int n) {
         swap(arr[minIndex], arr[i]);
     }
 }
-
+//selection sort is an in-place comparison sorting algorithm. It has an O(n^2) time complexity, which makes it inefficient on large lists, and generally performs worse than the similar insertion sort. Selection sort is noted for its simplicity and has performance advantages over more complicated algorithms in certain situations, particularly where auxiliary memory is limited.
 
 int main(){
     int arr[5]= {64,25,12,22,11};
